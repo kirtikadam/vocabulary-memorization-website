258c4eeff09d616683a7d1afcee473a66e5c5405
kirti kadam <kirti.kadam@excellarate.com>
2022-11-30 21:28:06 +0530

    project setup initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { CssBaseline, Box, Paper, Typography } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+        >
+          <Typography color="primary.dark" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
